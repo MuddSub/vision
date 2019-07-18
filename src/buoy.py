8acd5242223545d191efbdd304d66ed19184e402
Daniel Yang <dhyang@hmc.edu>
2019-07-18 09:45:06 -0700

    improvements

--- src/buoy.py
+++ src/buoy.py
@@ -133,8 +133,10 @@ class Buoy:
     def binarization(self,gray):
         ret, thresh1 = cv2.threshold(gray,255, 255, cv2.THRESH_BINARY)
         #cv2.imshow("skjfhsj",gray)
+
         ret2,th2 = cv2.threshold(gray,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        #th2 = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,-10)
+        gray = cv2.GaussianBlur(gray,(5,5),0)
+        th2 = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,51,-50)
         th2 = cv2.bitwise_not(th2)
         return th2
 
@@ -368,15 +370,16 @@ class Buoy:
         #binarization
 
         b,g,r = cv2.split(segmented)
-        newImg1 = b
-        newImg1 = cv2.cvtColor(segmented, cv2.COLOR_BGR2GRAY)
+        newImg1 = r
+        #newImg1 = cv2.cvtColor(segmented, cv2.COLOR_BGR2GRAY)
 
         cv2.imshow("before",newImg1)
         newImg1 = self.binarization(newImg1)
-        #newImg1 = cv2.morphologyEx(newImg1, cv2.MORPH_OPEN, np.ones((5,5)))
-        newImg1 = cv2.bitwise_not(mask)
+        newImg1 = cv2.morphologyEx(newImg1, cv2.MORPH_OPEN, np.ones((5,5)))
 
         cv2.imshow("after",newImg1)
+        newImg1 = cv2.bitwise_not(mask)
+
         #newImg1 = floodfill(newImg1)
         #newImg1 = fill(o1,newImg1)
         #newImg1 = cv2.cvtColor(newImg1, cv2.COLOR_BGR2GRAY)
